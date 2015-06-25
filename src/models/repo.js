72b17b928ed4aa922b6e0b698114c2518f30d48a
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-25 16:06:53 -0500

    Thu Jun 25 16:06:53 CDT 2015

--- src/models/repo.js
+++ src/models/repo.js
@@ -1,6 +1,7 @@
 import Model from 'ampersand-model'
+import githubMixin from '../helpers/github-mixin'
 
-export default Model.extend({
+export default Model.extend(githubMixin, {
   url () {
     return 'https://api.github.com/repos/' + this.full_name
   },
