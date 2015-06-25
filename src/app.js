d874b71e1f2b2c1fef14b4a62b9083f1cf6d490b
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-25 15:37:51 -0500

    Thu Jun 25 15:37:51 CDT 2015

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Router from './router'
 import styles from './styles/main.styl'
+import icons from 'octicons/octicons/octicons.css'
 import app from 'ampersand-app'
 import Me from './models/me'
 
