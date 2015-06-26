f4bbad2bde26fbc4599d3b1438306fe825683186
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-26 10:21:39 -0500

    Fri Jun 26 10:21:39 CDT 2015

--- src/pages/repo-detail.js
+++ src/pages/repo-detail.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
 
 export default React.createClass({
+  mixins: [ampersandMixin],
+
   displayName: 'RepoDetail',
 
   render () {
-    const {repo} = this.props
+    const {repo, labels} = this.props
 
     return (
       <div className='container'>
         <h1>{repo.full_name}</h1>
         <p></p>
-        <ul></ul>
+        <ul>
+          {labels.map((label) => {
+            return <li>{label.name}</li>
+          })}
+        </ul>
       </div>
     )
   }
--- src/router.js
+++ src/router.js
@@ -40,7 +40,7 @@ export default Router.extend({
 
   repoDetail (owner, name) {
     const model = app.me.repos.getByFullName(owner + '/' + name)
-    this.renderPage(<RepoDetail repo={model}/>)
+    this.renderPage(<RepoDetail repo={model} labels={model.labels}/>)
   },
 
   login () {
