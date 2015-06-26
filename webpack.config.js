ec38e2ea23d23df6315d60da98c9dfbfc9768a4f
Henrik Joreteg <hjoreteg@gmail.com>
2015-06-26 14:42:31 -0500

    Fri Jun 26 14:42:31 CDT 2015

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ require('babel/register')
 var getConfig = require('hjs-webpack')
 var React = require('react')
 var PublicPage = require('./src/pages/public')
+var Layout = require('./src/layout')
 
 module.exports = getConfig({
 	in: 'src/app.js',
@@ -9,10 +10,11 @@ module.exports = getConfig({
   clearBeforeBuild: true,
   html: function (context) {
     const publicPage = React.renderToString(React.createElement(PublicPage))
+    const layoutPage = React.renderToString(React.createElement(Layout, {me: {}}))
 
     return {
       'index.html': context.defaultTemplate({html: publicPage}),
-      '200.html': context.defaultTemplate()
+      '200.html': context.defaultTemplate({html: layoutPage})
     }
   }
 })
