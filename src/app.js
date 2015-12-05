654700503d5314f8a9dd12ebb459a1f690855cd4
Henrik Joreteg <hjoreteg@gmail.com>
2015-12-04 18:42:01 -0800

    start with "bare" version

--- src/app.js
+++ src/app.js
@@ -1,18 +1,2 @@
-import Router from './router'
-import styles from './styles/main.styl'
-import icons from 'octicons/octicons/octicons.css'
-import app from 'ampersand-app'
-import Me from './models/me'
+document.write('Hello Frontend Masters!')
 
-window.app = app
-
-app.extend({
-  init () {
-    this.me = new Me()
-    this.me.fetchInitialData()
-    this.router = new Router()
-    this.router.history.start()
-  }
-})
-
-app.init()
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,7 @@
-require('babel/register')
 var getConfig = require('hjs-webpack')
-var React = require('react')
-var PublicPage = require('./src/pages/public')
-var Layout = require('./src/layout')
 
 module.exports = getConfig({
-	in: 'src/app.js',
+  in: 'src/app.js',
   out: 'public',
-  clearBeforeBuild: true,
-  html: function (context) {
-    const publicPage = React.renderToString(React.createElement(PublicPage))
-    const layoutPage = React.renderToString(React.createElement(Layout, {me: {}}))
-
-    return {
-      'index.html': context.defaultTemplate({html: publicPage}),
-      '200.html': context.defaultTemplate({html: layoutPage})
-    }
-  }
+  clearBeforeBuild: true
 })
